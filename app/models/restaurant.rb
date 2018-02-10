743a551fd0ad7a68c311061b49bede696bab1473
jstooley <jstooley823@gmail.com>
2018-02-10 00:46:06 -0500

    added relationships to models

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,6 @@
 class Restaurant < ActiveRecord::Base
   has_many :reviews
+  has_many :users, through: :reviews
+  has_many :restaurant_foods
+  has_many :food_types, through: :restaurant_foods
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
-
   has_many :reviews
-
+  has_many :restaurants, through: :restaurant_foods
   validates :password, presence: true
 end
