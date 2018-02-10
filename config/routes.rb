03f78fb17b6992a648800592b838f7e8152e88e1
jstooley <jstooley823@gmail.com>
2018-02-10 01:20:55 -0500

    added controler files

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
+
   root 'welcome#home'
+
   resources :users
   resources :restaurants
   resources :reviews
   resources :food_types
+
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   post '/logout' => 'sessions#destroy'
+
 end
