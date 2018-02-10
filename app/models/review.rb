c621c159b92d0047e23e1ee7fd641f0969995ddb
jstooley <jstooley823@gmail.com>
2018-02-10 00:57:02 -0500

    added validations for review

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,8 @@
 class Review < ActiveRecord::Base
   belongs_to :user
   belongs_to :restaurant
+
+  validates :title, presence: true
+  validates :content, presence: true
+  validates :rating, inclusion: { in: 0..5 }
 end
