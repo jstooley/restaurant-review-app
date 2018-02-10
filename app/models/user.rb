b0759adcb7a89714abbf0fda5eaac047f8ab21ef
jstooley <jstooley823@gmail.com>
2018-02-10 00:52:19 -0500

    added validations for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   has_many :reviews
   has_many :restaurants, through: :restaurant_foods
   validates :password, presence: true
+  validates :username, presence: true
+  validates :username, uniqueness: true
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
 end
