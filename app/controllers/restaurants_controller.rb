28dbb8cb592aa7d5aaf45201101be624d049ab37
jstooley <jstooley823@gmail.com>
2018-02-11 00:20:58 -0500

    added restaurant and review controller action

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,18 +1,22 @@
 class RestaurantsController < ApplicationController
 
   def index
-
+    @restaurants = Restaurant.all
   end
 
   def show
-
+    @restaurant = Restaurant.find_by(id: params[:id])
   end
 
   def new
-
+    @restaurant = Restaurant.new
   end
 
   def create
+
+  end
+
+  def destroy
 
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,19 @@ Rails.application.routes.draw do
 
   resources :users
   resources :restaurants
-  resources :reviews
+  resources :reviews, only[:index, :show]
   resources :food_types
 
   get '/signup' => 'users#new'
   post '/signup' => 'users#create'
+  get '/:username', to: 'users#show'
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   post '/logout' => 'sessions#destroy'
-  get '/:username', to: 'users#show'
+
+  resources :users do
+    resources :reviews, only: [:new, :create, :edit, :update, :destroy]
+  end
+
 
  end
