c07dbe88388cd196b3eb12da68e6f1c1a6443eba
jstooley <jstooley823@gmail.com>
2018-02-12 17:54:23 -0500

    removed file

--- config/initializers/omniauth.rb
+++
@@ -1,3 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, ENV['GITHUB_KEY = 9802ce5ba57ea3da8a7c'], ENV['GITHUB_SECRET = 921f940eb3c0d0af5b0419f75ebe025b69901cbf']
-end
